4a6eb5f52ba604837004f79cb4099aabec3890f6
sagarg <info@sagarg.com.np>
2019-02-17 14:46:33 +0545

    Updates

---
+++ precache-manifest.47e0023138a87eee6d0575622fc78473.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff9313459badff1dcd14",
+    "url": "/Open-Data-Fellowship/static/js/runtime~main.ff931345.js"
+  },
+  {
+    "revision": "629381d1fbf3fbcf98eb",
+    "url": "/Open-Data-Fellowship/static/js/main.629381d1.chunk.js"
+  },
+  {
+    "revision": "aab3104b35f850e87814",
+    "url": "/Open-Data-Fellowship/static/js/1.aab3104b.chunk.js"
+  },
+  {
+    "revision": "629381d1fbf3fbcf98eb",
+    "url": "/Open-Data-Fellowship/static/css/main.1cfabd25.chunk.css"
+  },
+  {
+    "revision": "ed5bb20ba7e0a55e443eefe9cdc857c3",
+    "url": "/Open-Data-Fellowship/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Open-Data-Fellowship/precache-manifest.47e0023138a87eee6d0575622fc78473.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Open-Data-Fellowship/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
