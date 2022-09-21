4d6df2483a467b4c6d00b7f44494c0bd54d4d2bc
Zaymes <zaymesshrestha@gmail.com>
2022-09-21 05:57:38 +0545

    Updates

--- precache-manifest.03930affd90a3b5aec677e90a27ca3fa.js
+++ precache-manifest.03930affd90a3b5aec677e90a27ca3fa.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "404405b9825f2d7d93f9",
+    "revision": "e7607a6fc24923a14262",
     "url": "/datafellowship/static/css/main.787a06f4.chunk.css"
   },
   {
-    "revision": "404405b9825f2d7d93f9",
-    "url": "/datafellowship/static/js/main.404405b9.chunk.js"
+    "revision": "e7607a6fc24923a14262",
+    "url": "/datafellowship/static/js/main.e7607a6f.chunk.js"
   },
   {
     "revision": "e49ae6d4fcf009fa5a1c",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/datafellowship/static/js/runtime~main.f1b3d037.js"
   },
   {
-    "revision": "f2ad92f8f2167afae1953f236da19d3c",
+    "revision": "ed715899d2f07c95bc70e23a4eb2ac2a",
     "url": "/datafellowship/index.html"
   }
 ];
